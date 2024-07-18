b40f0f6f90d8bff4fd6e4b96e7cbdcb1f7fc7b4f
Mememan1616 <pm0l000054@gmail.com>
2024-07-18 08:27:11 -0600

    Limpieza de proyecto Rutas y Archivos

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ def index():
     lista=["alex","martin","Luz"]
     return render_template("index.html", titulo=titulo , lista=lista)
 
-
-@app.route("/saludo")
-def hello():
-    return "<h1>Hello my world</h1>"
-
 @app.route("/usuarios",methods=['GET','POST'])
 def usuarios():
     alum_form=forms.UserForm(request.form)
@@ -29,46 +24,6 @@ def usuarios():
         return render_template("usuarios.html",form=alum_form,email=email,matricula=matricula)
         
     return render_template('usuarios.html',form=alum_form)
-
-
-@app.route("/formulario1")
-def formulario1():
-    return render_template("formulario1.html")
-
-@app.route("/formulario2")
-def formulario2():
-    return render_template("formulario2.html")
-
-
-@app.route("/formulario3", methods=['GET','POST'])
-def formulario3():
-    num1=0
-    num2=0
-    if request.method=='POST':
-        num1= int (request.form.get('num1'))
-        num2= int (request.form.get('num2'))
-       
-        return render_template("formulario3.html",num1=num1,num2=num2)
-
-    return render_template("formulario3.html",num1=num1,num2=num2)
-    
-        
-
-    
-
-
-
-
-
-
-
-
-@app.route("/imprime", methods=['post'])
-def imprime():
-    nom=request.form.get("nom")
-    password=request.form.get("password")
-    return render_template("imprime.html", nom=nom, password=password)
-
 
 if __name__=="__main__":
     app.run(debug=True)
